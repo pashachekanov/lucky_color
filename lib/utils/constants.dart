5378cb3fd73bcf73e522b83fda5ded14c586ee34
Pavlo Chekanov <pasha.chekanov@gmail.com>
2025-06-11 23:22:02 +0300

    add constants class

---
+++ lib/utils/constants.dart
@@ -0,0 +1,21 @@
+/// Class that represents constant values
+class Constants {
+  /// Default [String] for home page
+  static const homeText = 'Hello There';
+
+  /// Default value for ARGB, with 0 being transparent
+  /// and 255 being fully opaque.
+  static const alphaValue = 255;
+
+  /// Max value[255]for red
+  static const maxRedIntValue = 255;
+
+  /// Max value[255]for green
+  static const maxGreenIntValue = 255;
+
+  /// Max value[255]for blue
+  static const maxBlueIntValue = 255;
+
+  /// Sensitivity for swipe
+  static const int swipeSensitivity = 0;
+}
